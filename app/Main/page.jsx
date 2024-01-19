ad6138f02a7cf2c106a62f70511f4341b7538d3c
Divine Agbasimelo <divinegodwin172@gmail.com>
2024-01-19 20:15:14 +0100

    ui adjusted

--- app/Main/page.jsx
+++ app/Main/page.jsx
@@ -9,11 +9,12 @@ const Main = () => {
     <div>
       <Navbar />
       <div>
-        <div className="w-[95%] ml-2 px-2 py-5 rounded-xl h-[110px] mt-[2rem] bg-[#023E8A]">
-          <div className="flex gap-3">
-            <p className="text-white text-[15px]">Available Balance</p>
+        <div className="w-[95%] ml-2  py-5  rounded-xl h-[110px] mt-[2rem] bg-[#023E8A]">
+          <div className="flex gap-1 justify-center">
+            <div className="flex gap-1 flex-row justfy-center ">
+            <p className="text-white text-[13px]">Available Balance</p>
             <svg
-              className="w-[18px] text-white"
+              className="w-[13px] text-white"
               data-slot="icon"
               fill="none"
               stroke-width="1.5"
@@ -28,16 +29,17 @@ const Main = () => {
                 d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
               ></path>
             </svg>
+            </div>
 
-            <div className="flex pl-12">
-              <p className="text-white text-[15px]"> Transaction details</p>
+            <div className="flex pl-12 px-3">
+              <p className="text-white text-[13px]"> Transaction details</p>
             </div>
           </div>
 
-          <div className="flex flex-row gap-[1rem] py-2">
-            <p className="text-white text-3xl py-2 font-bold"> {userAmount}</p>
+          <div className="flex gap-1 py-2 justify-center px-3">
+            <p className="text-white text-3xl py-2 pl-1 font-bold"> {userAmount}</p>
             <Button
-              className="w-[120px] h-[40px] mt-2 ml-[5rem] bg-white text-black rounded-full"
+              className="w-[120px] h-[40px] mt-2 ml-[3rem] bg-white text-black rounded-full"
               value="add money"
             />
           </div>
