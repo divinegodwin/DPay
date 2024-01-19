bd6b7f95583e33200d95c8945ae9925d0eba68d2
Divine Agbasimelo <divinegodwin172@gmail.com>
2024-01-19 23:54:46 +0100

    new

---
+++ app/Card/page.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Button from '../Components/Button'
+
+const card = () => {
+
+  return (
+    <div>
+
+<div className='mt-5 pl-4 flex flex-row gap-40'>
+
+<header className='text-2xl font-bold'>Cards</header>
+
+<p className='text-lg text-[#0077b6] pl-16 pt-1'>Q&A</p>
+
+</div>
+
+<div className='flex flex-col justify-center'>
+<p className='Font-bold text-center pt-[2rem] font-bold'>Virtual Card</p>
+</div>
+<div className='flex justify-center'>
+<div className='w-[40px] flex h-[3px] rounded-full bg-[#0077b6]'></div>
+</div>
+
+
+<div className='grid justify-center'>
+<header className='text-white font-bold text-xl relative top-24 right-[5rem]'>DPay</header>
+<p className='text-white  relative top-[14rem] right-[5rem]'>Virtual Card</p>
+</div>
+
+<div className='w-[80%] h-[200px] bg-[#023E8A] flex justify-center ml-9 rounded-xl mt-10' >
+</div>
+
+
+<div className='flex justify-center mt-28'>
+  <Button className='w-[300px] h-[60px] rounded-full font-bold bg-[#023E8A] text-white' value='Get It Now' />
+</div>
+
+
+    </div>
+  )
+}
+
+export default card
--- app/Components/Footer.jsx
+++ app/Components/Footer.jsx
@@ -51,6 +51,7 @@ const Footer = () => {
 
 
 <div>
+  <Link href='/Card'>
         <svg
           className="w-[30px]"
           data-slot="icon"
@@ -67,6 +68,7 @@ const Footer = () => {
             d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z"
           ></path>
         </svg>
+        </Link>
         </div>
 
         <div>
