fd2915c55345b00d677264565dc962e47e0d3b89
Divine Agbasimelo <divinegodwin172@gmail.com>
2024-01-27 20:30:08 +0100

    modified context

--- app/layout.js
+++ app/layout.js
@@ -13,22 +13,22 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-   
+
     <html lang="en">
 
 
-  
+
       <body className={inter.className}>
-      <UserProvider>
-        {children}
-    
-      <Footer />
-      </UserProvider>
+        <UserProvider>
+          {children}
+
+          <Footer />
+        </UserProvider>
       </body>
-      
-     
+
+
     </html>
 
- 
+
   )
 }
